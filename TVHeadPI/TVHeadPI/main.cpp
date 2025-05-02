dbf2430b589c136e343b470824da1be2d30b6074
MushJam64 <100545041+MushJam64@users.noreply.github.com>
2025-05-02 14:23:41 -0400

    so retro

--- TVHeadPI/TVHeadPI/main.cpp
+++ TVHeadPI/TVHeadPI/main.cpp
@@ -2,23 +2,19 @@
 #include <SDL3/SDL_main.h>
 #include <SDL3_image/SDL_image.h>
 #include <SDL3/SDL_joystick.h>
-#include <stdio.h> 
 #include <SDL3/SDL_video.h>
+#include <vector>
+#include <stdio.h>
 
 SDL_Window* window;
 SDL_Renderer* renderer;
-SDL_Surface* eye_surface;
 SDL_Texture* eye_texture;
-SDL_FRect eye_position;
-SDL_Surface* mouth_surface;
 SDL_Texture* mouth_texture;
+SDL_FRect eye_position;
 SDL_Event event;
 SDL_Joystick* joystick;
 
-int can_blink = true;
-int blinktimer;
-int total;
-int i;
+int blinktimer = 0;
 
 enum EyeExpression {
     NONE,
@@ -29,6 +25,15 @@ enum EyeExpression {
 };
 
 EyeExpression manual_expression = NONE;
+bool konami_active = false;
+
+std::vector<int> input_buffer;
+const std::vector<int> konami_code = {12, 12, 13, 13, 14, 15, 14, 15, 1, 0};
+
+IMG_Animation* gif_animation = nullptr;
+int gif_frame = 0;
+Uint64 gif_last_time = 0;
+bool gif_loaded = false;
 
 void init_joystick() {
     SDL_Init(SDL_INIT_JOYSTICK);
@@ -40,102 +45,47 @@ void init_joystick() {
 }
 
 void check_blinking() {
-    total = SDL_GetNumJoystickButtons(joystick);
-
-    if (SDL_GetJoystickButton(joystick, 0)) {
-        manual_expression = HAPPY;
-    }
-    else if (SDL_GetJoystickButton(joystick, 1)) {
-        manual_expression = SAD;
-    }
-    else if (SDL_GetJoystickButton(joystick, 2)) {
-        manual_expression = ANGRY;
-    }
-    else if (SDL_GetJoystickButton(joystick, 3)) {
-        manual_expression = SURPRISED;
-    }
-    else {
-        manual_expression = NONE;
-    }
+    if (SDL_GetJoystickButton(joystick, 0)) manual_expression = HAPPY;
+    else if (SDL_GetJoystickButton(joystick, 1)) manual_expression = SAD;
+    else if (SDL_GetJoystickButton(joystick, 2)) manual_expression = ANGRY;
+    else if (SDL_GetJoystickButton(joystick, 3)) manual_expression = SURPRISED;
+    else manual_expression = NONE;
 
     if (manual_expression == NONE) {
-        can_blink = true;
         blinktimer++;
-        if (blinktimer > 860) {
-            blinktimer = 0;
-        }
-    }
-    else {
-        can_blink = false;
+        if (blinktimer > 860) blinktimer = 0;
+    } else {
         blinktimer = 0;
     }
 }
 
-
 void check_rendering_eye_states() {
     check_blinking();
-
-    // Determine which eye image to load based on blink timer
-    const char* image_path = nullptr;
-
-    if (can_blink) {
-        if ((blinktimer >= 0 && blinktimer < 15) || (blinktimer >= 60)) {
-            image_path = "./images/test_eyes.png";
-        } else if ((blinktimer >= 15 && blinktimer < 30) || (blinktimer >= 45 && blinktimer <= 60)) {
-            image_path = "./images/test_half.png";
-        } else if (blinktimer > 30 && blinktimer < 45) {
-            image_path = "./images/test_closed.png";
-        }
-    } 
+    const char* path = nullptr;
 
     if (manual_expression != NONE) {
-        const char* path = nullptr;
-
         switch (manual_expression) {
-        case HAPPY:
-            path = "./images/test_happy.png";
-            break;
-        case SAD:
-            path = "./images/test_sad.png";
-            break;
-        case ANGRY:
-            path = "./images/test_angry.png";
-            break;
-        case SURPRISED:
-            path = "./images/test_surprised.png";
-            break;
-        default:
-            break;
+            case HAPPY: path = "./images/test_happy.png"; break;
+            case SAD: path = "./images/test_sad.png"; break;
+            case ANGRY: path = "./images/test_angry.png"; break;
+            case SURPRISED: path = "./images/test_surprised.png"; break;
+            default: break;
         }
-
-        if (path) {
-            SDL_Surface* expression_surface = IMG_Load(path);
-            SDL_Texture* expression_texture = SDL_CreateTextureFromSurface(renderer, expression_surface);
-            SDL_DestroySurface(expression_surface);
-            if (eye_texture) SDL_DestroyTexture(eye_texture);
-            eye_texture = expression_texture;
-        }
+    } else {
+        if ((blinktimer >= 0 && blinktimer < 60) || (blinktimer >= 240))
+            path = "./images/test_eyes.png";
+        else if ((blinktimer >= 60 && blinktimer < 120) || (blinktimer >= 180 && blinktimer <= 240))
+            path = "./images/test_half.png";
+        else if (blinktimer > 120 && blinktimer < 180)
+            path = "./images/test_closed.png";
     }
 
-    if (image_path) {
-        SDL_Surface* new_surface = IMG_Load(image_path);
-        if (!new_surface) {
-            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to load image %s: %s", image_path, SDL_GetError());
-            return;
-        }
-
-        SDL_Texture* new_texture = SDL_CreateTextureFromSurface(renderer, new_surface);
-        SDL_DestroySurface(new_surface);
-        if (!new_texture) {
-            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to create texture from %s: %s", image_path, SDL_GetError());
-            return;
-        }
-
-        // Replace the old eye texture
-        if (eye_texture) {
-            SDL_DestroyTexture(eye_texture);
-        }
-        eye_texture = new_texture;
+    if (path) {
+        SDL_Surface* surf = IMG_Load(path);
+        SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, surf);
+        SDL_DestroySurface(surf);
+        if (eye_texture) SDL_DestroyTexture(eye_texture);
+        eye_texture = tex;
     }
 }
 
@@ -145,83 +95,88 @@ float get_axis(int index) {
     return SDL_GetJoystickAxis(joystick, index);
 }
 
-int controller_num_buttons() {
-    if (!joystick) return 0;
-    return SDL_GetNumJoystickButtons(joystick);
-}
-
-int main(int argc, char* argv[])
-{   
-
+int main(int argc, char* argv[]) {
     init_joystick();
 
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
-        return 3;
+        return 1;
     }
 
-    if (!SDL_CreateWindowAndRenderer("CRT STELLE 0.1.2", 1280, 640, SDL_WINDOW_FULLSCREEN, &window, &renderer)) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
-        return 3;
+    if (!SDL_CreateWindowAndRenderer("SDL Konami", 1280, 640, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window/renderer: %s", SDL_GetError());
+        return 1;
     }
 
-    eye_surface = IMG_Load("./images/test_eyes.png");
-    if (!eye_surface) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", SDL_GetError());
-        return 3;
-    }
-    
-    eye_texture = SDL_CreateTextureFromSurface(renderer, eye_surface);
-    if (!eye_texture) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
-        return 3;
-    }
-    SDL_DestroySurface(eye_surface);
+    SDL_Surface* mouth_surf = IMG_Load("./images/test_mouth.png");
+    mouth_texture = SDL_CreateTextureFromSurface(renderer, mouth_surf);
+    SDL_DestroySurface(mouth_surf);
 
-    mouth_surface = IMG_Load("./images/test_mouth.png");
-    if (!mouth_surface) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", SDL_GetError());
-        return 3;
+    gif_animation = IMG_LoadAnimation("./images/retro.gif");
+    if (gif_animation) {
+        gif_loaded = true;
     }
-    mouth_texture = SDL_CreateTextureFromSurface(renderer, mouth_surface);
-    if (!mouth_texture) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
-        return 3;
-    }
-    SDL_DestroySurface(mouth_surface);
-    SDL_SetRenderLogicalPresentation(renderer, 1280, 640, SDL_LOGICAL_PRESENTATION_INTEGER_SCALE);
 
     while (1) {
-        SDL_PollEvent(&event);
-        if (event.type == SDL_EVENT_QUIT) {
-            break;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) return 0;
+
+            if (event.type == SDL_EVENT_JOYSTICK_BUTTON_DOWN) {
+                input_buffer.push_back(event.jbutton.button);
+                if (input_buffer.size() > konami_code.size()) {
+                    input_buffer.erase(input_buffer.begin());
+                }
+                if (input_buffer == konami_code) {
+                    konami_active = true;
+                    gif_frame = 0;
+                    gif_last_time = SDL_GetTicks();
+                }
+            }
+
+            if (event.type == SDL_EVENT_KEY_DOWN) {
+                /* the pressed key was Escape? */
+                if (event.key.key == SDLK_ESCAPE) {
+                    konami_active = true;
+                    gif_frame = 0;
+                    gif_last_time = SDL_GetTicks();
+                }
+            }
         }
+
         eye_position.w = 1280;
         eye_position.h = 640;
-        
+        eye_position.x = get_axis(0) / 32767.0f * 25;
+        eye_position.y = get_axis(1) / 32767.0f * 25;
+
+        if (konami_active && gif_loaded) {
+            Uint64 now = SDL_GetTicks();
+            if (now - gif_last_time > gif_animation->delays[gif_frame]) {
+                gif_frame = (gif_frame + 1) % gif_animation->count;
+                gif_last_time = now;
+            }
+
+            SDL_Texture* frame_texture = SDL_CreateTextureFromSurface(renderer, gif_animation->frames[gif_frame]);
+            SDL_RenderClear(renderer);
+            SDL_RenderTexture(renderer, frame_texture, NULL, NULL);
+            SDL_RenderPresent(renderer);
+            SDL_DestroyTexture(frame_texture);
+            continue;
+        }
 
         check_rendering_eye_states();
 
-     //   if (joystick) {
-            eye_position.x = get_axis(0) / 32767.0f * 25;
-
-            eye_position.y = get_axis(1) / 32767.0f * 25;
-    //    }
-
-        printf("BlinkTimer: %d\n", blinktimer);
-
-        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
         SDL_RenderTexture(renderer, eye_texture, NULL, &eye_position);
         SDL_RenderTexture(renderer, mouth_texture, NULL, NULL);
         SDL_RenderPresent(renderer);
     }
 
-    SDL_DestroyTexture(eye_texture);
-    SDL_DestroyTexture(mouth_texture);
+    if (eye_texture) SDL_DestroyTexture(eye_texture);
+    if (mouth_texture) SDL_DestroyTexture(mouth_texture);
+    if (gif_loaded) IMG_FreeAnimation(gif_animation);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-
     SDL_Quit();
 
     return 0;
